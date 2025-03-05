c8b068bb9c8fa8d3542c3bbeb63ca73c2e7360e7
Tommy Son <tommy.m.son@gmail.com>
2025-03-05 03:06:35 -0800

    Make properties of CreateMovieRequest to required

--- Movies.Contracts/Requests/CreateMovieRequest.cs
+++ Movies.Contracts/Requests/CreateMovieRequest.cs
@@ -2,7 +2,7 @@ namespace Movies.Contracts.Requests;
 
 public class CreateMovieRequest
 {
-    public string Title { get; set; }
-    public int YearOfRelease { get; set; }
-    public IEnumerable<string> Genres { get; set; }
+    public required string Title { get; init; }
+    public required int YearOfRelease { get; init; }
+    public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
 }
