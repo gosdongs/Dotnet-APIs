9e7f942ac1918f84a5a2da3c43c9df236e458565
Tommy Son <tommy.m.son@gmail.com>
2025-03-09 23:14:12 -0700

    Define methods for MovieRepository interface, implement stubs

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -1,6 +1,31 @@
+using Movies.Application.Models;
+
 namespace Movies.Application.Repositories;
 
 public class MovieRepository : IMovieRepository
 {
-    
+    public Task<bool> CreateAsync(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
