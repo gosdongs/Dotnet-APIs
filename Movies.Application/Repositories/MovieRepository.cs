65368fd9c1738b96300042c3b20e3d93f2ac7c58
Tommy Son <tommy.m.son@gmail.com>
2025-03-09 23:22:26 -0700

    Implement MovieRepository methods with temporary variable as database

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -4,28 +4,46 @@ namespace Movies.Application.Repositories;
 
 public class MovieRepository : IMovieRepository
 {
+    private readonly List<Movie> _movies = [];
+    
     public Task<bool> CreateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        _movies.Add(movie);
+
+        return Task.FromResult(true);
     }
 
     public Task<Movie?> GetByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var movie = _movies.SingleOrDefault(m => m.Id == id);
+
+        return Task.FromResult(movie);
     }
 
     public Task<IEnumerable<Movie>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return Task.FromResult(_movies.AsEnumerable());
     }
 
     public Task<bool> UpdateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        var movieIndex = _movies.FindIndex(m => m.Id == movie.Id);
+
+        if (movieIndex == -1)
+        {
+            return Task.FromResult(false);
+        }
+
+        _movies[movieIndex] = movie;
+        
+        return Task.FromResult(true);
     }
 
     public Task<bool> DeleteByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var removedCount = _movies.RemoveAll(m => m.Id == id);
+        var isMovieRemoved = removedCount > 0;
+
+        return Task.FromResult(isMovieRemoved);
     }
 }
