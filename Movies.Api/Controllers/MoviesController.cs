f4ba52d09c3ad6c05ca45d2aedef66d770a233ff
Tommy Son <tommy.m.son@gmail.com>
2025-03-10 16:24:01 -0700

    Add temporary Create route

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
 
 namespace Movies.Api.Controllers;
 
 [ApiController]
+[Route("api")]
 public class MoviesController : ControllerBase
 {
     private readonly IMovieRepository _movieRepository;
@@ -11,5 +13,11 @@ public class MoviesController : ControllerBase
     public MoviesController(IMovieRepository movieRepository)
     {
         _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+    {
+        return Ok(request);
     }
 }
