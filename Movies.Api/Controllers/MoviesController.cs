94bb4c95f16810d2269d24cab8aabd0e57ebb68e
Tommy Son <tommy.m.son@gmail.com>
2025-03-10 17:36:22 -0700

    Replace instantiating Movie model in controller with custom mapper

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Movies.Application.Models;
+using Movies.Api.Mapping;
 using Movies.Application.Repositories;
 using Movies.Contracts.Requests;
 
@@ -19,13 +19,7 @@ public class MoviesController : ControllerBase
     [HttpPost("movies")]
     public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
     {
-        var movie = new Movie
-        {
-            Id = Guid.NewGuid(),
-            Title = request.Title,
-            YearOfRelease = request.YearOfRelease,
-            Genres = request.Genres.ToList()
-        };
+        var movie = request.MapToMovie();
         
         var result = await _movieRepository.CreateAsync(movie);
 
