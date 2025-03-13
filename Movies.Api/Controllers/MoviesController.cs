95d9dc1b5575e7f03d4fcfafe30dad3d3286e36f
Tommy Son <tommy.m.son@gmail.com>
2025-03-12 22:55:07 -0700

    Implement Delete endpoint

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -60,4 +60,17 @@ public class MoviesController : ControllerBase
 
         return Ok(movie.MapToResponse());
     }
+
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var deleted = await _movieRepository.DeleteByIdAsync(id);
+        
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
 }
