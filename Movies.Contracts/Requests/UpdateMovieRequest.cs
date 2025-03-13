50e9aaf769a2427d7b5a13feeec5c77a79c223ea
Tommy Son <tommy.m.son@gmail.com>
2025-03-12 23:13:20 -0700

    Add Slug in Movie model

--- Movies.Contracts/Requests/UpdateMovieRequest.cs
+++ Movies.Contracts/Requests/UpdateMovieRequest.cs
@@ -3,6 +3,7 @@ namespace Movies.Contracts.Requests;
 public class UpdateMovieRequest
 {
     public required string Title { get; init; }
+    public required string Slug { get; set; }
     public required int YearOfRelease { get; init; }
     public required IEnumerable<string> Genres { get; init; } = [];
 }
